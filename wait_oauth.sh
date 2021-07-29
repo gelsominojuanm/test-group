fcb0dd2a04c410af480f4e55b9856d2fc4b9ef68
gelsominojuanm <gelsominojuanm@gmail.com>
2021-07-29 16:18:16 -0300

    cambios

--- wait_oauth.sh
+++ wait_oauth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
   
 oc wait co/authentication --for condition=Progressing \
-    --timeout 120s
+    --timeout 150s
 oc rollout status -n openshift-authentication deployment/oauth-openshift \
-    --timeout 120s
+    --timeout 150s
 while [ -n "$(oc get pod -n openshift-authentication -o \
 jsonpath='{.items[?(@.metadata.deletionTimestamp != "")].metadata.name}')" ]
 do
